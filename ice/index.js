db30ea44b40b3378d2962643fa4afa99ff70128c
Vic <vicyesixuan@gmail.com>
2022-02-08 16:13:08 +0800

    调整代码

--- ice/index.js
+++ ice/index.js
@@ -18,8 +18,8 @@ import * as THREE from './js/three.module.js';
 
 				scene = new THREE.Scene();
 
-				const envmap = new THREE.TextureLoader().load( "model/env.jpg",function(texture){
-					const loader = new GLTFLoader().setPath( 'model/' );
+				const envmap = new THREE.TextureLoader().load( "/env.jpg",function(texture){
+					const loader = new GLTFLoader().setPath( '/' );
 					loader.load( 'dwendwen.gltf', function ( gltf ) {
 						gltf.scene.traverse( function ( child ) {
 							if(child.name=="outer"||child.name=="mask"){
