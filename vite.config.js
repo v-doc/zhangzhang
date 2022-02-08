6ca632a969f4e194b0d676f432b909714c9e65fa
Vic <vicyesixuan@gmail.com>
2022-02-08 14:08:17 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path')
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'ice/index.html')
+      }
+    }
+  },
+  plugins: [react()]
+})
